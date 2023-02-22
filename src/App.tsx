202a3f0cf8542f708598d93604a1f7d3becdde1f
psyofrelief <phariedd@gmail.com>
2023-02-22 11:34:30 +0800

    Remove unused dependancies

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import { Routes, Route } from "react-router-dom";
 import Homepage from "./components/Homepage";
-import axios from "axios";
 import React, { useState } from "react";
 
 const App = () => {
